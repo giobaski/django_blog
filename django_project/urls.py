66567b353705579d07c9f69b0c0ee2352f4d8198
giobaski <giobaski@gmail.com>
2019-04-10 17:55:21 +0400

    profile

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 from users import views as user_views
 
 urlpatterns = [
@@ -30,3 +32,7 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
 
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
