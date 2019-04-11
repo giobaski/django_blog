d03f338ea08849c625b5355c3b3f79b679ace9d7
giobaski <giobaski@gmail.com>
2019-04-11 17:42:23 +0400

    update user profile

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Post
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class PostModelTest(TestCase):
+
+    def setUp(self):
+        user = User(username="testgeorge")
+        user.save()
+        Post.objects.create(title='testcase1', content="this is test content", author = user)
+
+    def test_title_content(self):
+        post = Post.objects.get(id=5)
+        expected_object_name = f'{post.title}'
+        self.assertEqual(expected_object_name, 'testcase1')
+
+
